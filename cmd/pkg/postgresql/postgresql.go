06f7b78c0a3a69e387d57d8c3a0fc953ecebdf01
Zavr22 <Mkulik2407@gmail.com>
2023-01-27 01:35:21 +0300

    [ADD] Postgres database

---
+++ cmd/pkg/postgresql/postgresql.go
@@ -0,0 +1,42 @@
+package postgresql
+
+import (
+	"context"
+	"fmt"
+	"github.com/Zavr22/goLangStudy/cmd/pkg/utills"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4"
+	pgxpool "github.com/jackc/pgx/v4/pgxpool"
+	"log"
+	"time"
+)
+
+type Client interface {
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+func NewClient(ctx context.Context, maxAttempts int, username, password, host, post, database string) (con *pgxpool.Pool, err error) {
+
+	dsn := fmt.Sprintf("postgresql//%s:%s@%s:%s/%s", username, password, host, post, database)
+	err = utills.DoWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		con, err = pgxpool.Connect(ctx, dsn)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, maxAttempts, 5*time.Second)
+
+	if err != nil {
+
+		log.Fatal("error ")
+	}
+
+	return con, nil
+}
